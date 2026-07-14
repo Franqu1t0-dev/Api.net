f36b54abb11b55a5834f7b4411c85a74e9dc7661
Franqu1t0-dev <fpueblacandia@frba.utn.edu.ar>
2026-07-14 20:03:29 -0300

    Creacionde categoryRepository

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
+var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql");
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(dbConnectionString));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
