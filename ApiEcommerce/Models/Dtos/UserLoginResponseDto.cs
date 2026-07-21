914d8531a73b6a22cbe7b11a6c98b92d34967548
Franqu1t0-dev <fpueblacandia@frba.utn.edu.ar>
2026-07-21 17:32:18 -0300

    Implementing login

--- ApiEcommerce/Models/Dtos/UserLoginResponseDto.cs
+++ ApiEcommerce/Models/Dtos/UserLoginResponseDto.cs
@@ -7,4 +7,6 @@ public class UserLoginResponseDto
 {
     public UserRegisterDto? User { get; set; }
     public string? Token { get; set; }
+
+    public string? Message {get; set;}
 }
--- ApiEcommerce/Models/Dtos/UserRegisterDto.cs
+++ ApiEcommerce/Models/Dtos/UserRegisterDto.cs
@@ -7,7 +7,7 @@ public class UserRegisterDto
 {
     public string? ID { get; set; }
     public string? Name { get; set; }
-    public required string UserName { get; set; }
+    public required string Username { get; set; }
     public required string Password { get; set; }
     public string? Role { get; set; }
 }
