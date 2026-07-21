035f520ef639062b74bb68bef71af17b13012926
Franqu1t0-dev <fpueblacandia@frba.utn.edu.ar>
2026-07-20 22:59:11 -0300

    Product delete

--- ApiEcommerce/obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
+++ ApiEcommerce/obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0034b57a00ba7c4d080c50c6c89b38d50c6956e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92e93f3f8f909a6eb2825ef3276cea09041a154")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
